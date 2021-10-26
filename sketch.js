370f7ab784a7248508fe2fbe9f062d74c49684f7
Anon853 <4m4zon3@web.de>
2021-10-26 23:06:20 +0200

    reworked timer, improved velocity

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ class Ball {
   constructor(x, v){
     this.x = x;
     this.y = 100;
-    this.velocity = v;
+    this.ballVelocity = v;
 }
   move(){
-    this.y = this.y - this.velocity;
+    this.y = this.y - this.ballVelocity;
   }
   display(){
     stroke(255);
@@ -26,51 +26,26 @@ class Planet {
   }
 }
 
-let xPos = 0;
-let yPos = 505;
-const velocity = [-2.7, -0.265, -0.4, -0.43, -0.178, -0.264, -0.62, -0.44, -0.4, -0.445, -0.11];
+
+const ballVelocity = [-2.76, -0.285, -0.45, -0.47, -0.187, -0.285, -0.745, -0.487, -0.44, -0.492, -0.113];
 const ballArray = [];
 const planetArray = [];
-let timeDisplay = "0:00";
-let timeCalc = 0;
+let timeDisplay = "0.0";
+let timeCalc;
 const planetImageArray = ['sun','mercury','venus','earth','moon','mars','jupiter','saturn','uranus','neptune','pluto'];
-const calcFunctionDataArray = [274, 3.7, 8.9, 9.8, 1.6, 3.7, 24.8, 10.4, 8.9, 11.2, 0.6];
+const calcGravityDataArray = [274, 3.7, 8.9, 9.8, 1.6, 3.7, 24.8, 10.4, 8.9, 11.2, 0.6];
 
 function setup (){
     createCanvas (1000, 600);
     textSize(20);
-
+    let xPos = 0;
+    let yPos = 505;
     //creates ball and planet data, fills arrays
-    for (let i = 0; i < velocity.length; i++) {
+    for (let i = 0; i < ballVelocity.length; i++) {
       xPos += 75;
-      ballArray[i] = new Ball(xPos, velocity[i]);  
+      ballArray[i] = new Ball(xPos, ballVelocity[i]);  
       planetArray[i] = new Planet(xPos, yPos, planetImageArray[i]);
     }
-
-    function startTimer(duration, display) {
-      let timer = duration, minutes, seconds;
-      setInterval(function () {
-          minutes = parseInt(timer / 60, 10)
-          seconds = parseInt(timer % 60, 10);
-          minutes = minutes < 10 ? + minutes : minutes;
-          seconds = seconds < 10 ? "0" + seconds : seconds;
-  
-          display.textContent = minutes + ":" + seconds;
-          timeDisplay = display.textContent;
-          timeCalc = parseInt(seconds);
-        
-          if (++timer < 0) {
-        timer = duration;
-          }
-    }, 1000);
-  }
-  function timer() {
-      let Minutes = 0,
-         display = document.getElementById("timer");
-      startTimer(Minutes, display);
-  };
-
-  timer();
 }
   
   function draw(){ 
@@ -81,9 +56,15 @@ function setup (){
     text("1 km", 900, 100);
     text("0 km", 900, 500);
     text("Gravity Fall Simulation", width/2 -100, 10);
-    text(timeDisplay + " tDisplay", 10, 20);
-    //text(timeCalc + " timecalc", 200, 20);
+    text("t = " + timeDisplay, 0, 80);
     fill(255, 255, 255);
+
+    function time(){
+      let time = millis();
+      timeCalc = time / 1000;
+      timeDisplay = time / 1000;
+    }
+    time();
 
     //draws falling balls and planets from array data
     ballArray.forEach(ballArray => {
@@ -91,7 +72,7 @@ function setup (){
       ballArray.display();
       
     if (ballArray.y > 490) {
-      ballArray.velocity = 0;   
+      ballArray.ballVelocity = 0;   
     }
   });
 
@@ -99,7 +80,7 @@ function setup (){
     planetArray.display();
 });
   //returns data of falling ball graphic
-  //[t] based on timer to re-calculate velocity each second of free fall 
+  //[t] based on timer to re-calculate ballVelocity each second of free fall 
   const calc = (g, t) => {
     //let d = 0;
     let v = 0;
@@ -110,34 +91,29 @@ function setup (){
     return returnArray;
   }
   
-  //inserts data into calc() from data array, displays return values, increments xTextPos pixel position for each planet
+  //inserts data into calc() from gravitation array, displays return values of calc(), increments xTextPos pixel position for each planet
   const calcInputAndDisplay = () => {
     let xTextPos = 0;
-  for (let i = 0; i < calcFunctionDataArray.length; i++) {
-    let results = calc(calcFunctionDataArray[i], timeCalc);
+  for (let i = 0; i < calcGravityDataArray.length; i++) {
+    let results = calc(calcGravityDataArray[i], timeCalc);
     xTextPos += 75;
 
     textSize(13);
     noStroke();
     fill(255, 255, 255);
-    text(Math.floor(results[0]) + "km/h", xTextPos, 50);
-  console.log(ballArray[1].velocity);
-      if(ballArray[i].velocity == 0) {
+    //text(Math.floor(results[0]) + "km/h", xTextPos, 50);
+      if(timeDisplay >= results[1]) {
+        text(Math.floor(results[0]) + "km/h", xTextPos, 50);
         fill(0, 255, 0);
-        text(results[1].toFixed(2) + " s", xTextPos, 80);    
-      }  else {
-        fill(255, 255, 255);
+        text(results[1].toFixed(2) + " s", xTextPos, 80);   
+        ballArray[i].ballVelocity = 0; 
+      }else{
+        fill(255, 0, 0);
+        text(timeDisplay + "s", xTextPos, 80);
       }
-    
-
-    
-  
-    
-    
     fill(255, 255, 255);
-    text("Gravity \n" + calcFunctionDataArray[i] + " m/s²", xTextPos -22, 580);
+    text("Gravity \n" + calcGravityDataArray[i] + " m/s²", xTextPos -22, 580);
   }
-  
 }
 calcInputAndDisplay();
 }
